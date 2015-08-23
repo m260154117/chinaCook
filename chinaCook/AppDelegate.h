bfab6647df4c8ac246eb678f8091babf212c6d7b
m260154117 <260154117@qq.com>
2015-08-23 15:48:38 +0800

    代码初始化

---
+++ chinaCook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chinaCook
+//
+//  Created by MiaoLizhuang on 15/8/23.
+//  Copyright (c) 2015年 MiaoLizhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chinaCook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chinaCook
+//
+//  Created by MiaoLizhuang on 15/8/23.
+//  Copyright (c) 2015年 MiaoLizhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
